a209c501eb6c5762d430aee9f8f8dfad8edf2646
hope1309 <luongtrieuvy1103@gmail.com>
2024-12-19 16:20:27 +0700

    Giao dien ADMIN

---
+++ ADMIN/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="./css/styleadmin.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <link rel="shortcut icon" type="image/png" href="/image/admin.jpg">
+    <title>Admin</title>
+</head>
+
+<body>
+    <div class="wrapper">
+        <?php
+        include("config/config.php");
+        include("./modules/menu.php");
+        include("./modules/main.php");
+
+        ?>
+    </div>
+</body>
+
+</html>
